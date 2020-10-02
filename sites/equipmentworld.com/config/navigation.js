bedb8b8c04ef2bbe49032f5b8766d292c1d6b1aa
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-02 14:58:23 -0500

    Add navigation.js

---
+++ sites/equipmentworld.com/config/navigation.js
@@ -0,0 +1,46 @@
+const dragonForms = require('./dragon-forms');
+
+module.exports = {
+  primary: {
+    items: [
+    ],
+  },
+  secondary: {
+    items: [
+    ],
+  },
+  tertiary: {
+    items: [
+      { href: '/search', label: 'Search', icon: 'search' },
+    ],
+  },
+  footer: {
+    items: [
+      { href: '/contact-us', label: 'Contact Us' },
+      { href: '/page/advertise', label: 'Advertise' },
+      { href: '/page/privacy-policy', label: 'Privacy Policy', target: '_blank' },
+      { href: '/page/terms-conditions', label: 'Terms & Conditions', target: '_blank' },
+    ],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [
+      ],
+    },
+    {
+      label: 'Resources',
+      items: [
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+        { href: '/contact-us', label: 'Contact Us' },
+        { href: '/page/advertise', label: 'Advertise' },
+        { href: '/page/privacy-policy', label: 'Privacy Policy', target: '_blank' },
+        { href: '/page/terms-conditions', label: 'Terms & Conditions', target: '_blank' },
+      ],
+    },
+  ],
+};
