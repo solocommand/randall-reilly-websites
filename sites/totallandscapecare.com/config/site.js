ba0713b4497dc010d2ccd79d50adebcd0786d47e
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-12 07:49:28 -0600

    Update logos to use svgs

--- sites/totallandscapecare.com/config/site.js
+++ sites/totallandscapecare.com/config/site.js
@@ -12,15 +12,15 @@ module.exports = {
   company: 'Randall-Reilly, LLC',
   logos: {
     navbar: {
-      src: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc_logo.png?h=45',
+      src: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc-logo.svg?h=45',
       srcset: [
-        'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc_logo.png?h=90 2x',
+        'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc-logo.svg?h=90 2x',
       ],
     },
     footer: {
-      src: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc_logo.png?h=60',
+      src: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc-logo.svg?h=60',
       srcset: [
-        'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc_logo.png?h=120 2x',
+        'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc-logo.svg?h=120 2x',
       ],
     },
   },
@@ -57,7 +57,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#222222',
-      logo: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc_logo.png?h=60',
+      logo: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc-logo.svg?h=60',
     },
     to: 'support@totallandscapecare.com',
   },
@@ -66,7 +66,7 @@ module.exports = {
   //   directSend: true,
   //   sendTo: 'support@parameter1.com',
   //   sendFrom: 'totallandscapecare.com <noreply@parameter1.com>',
-  //   logo: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc_logo.png?h=60',
+  //   logo: 'https://p1-cms-assets.imgix.net/files/base/randallreilly/all/image/static/tlc-logo.svg?h=60',
   //   bgColor: '#222222',
   // },
 };
