dc55e45b9c35509143eb2fe39de58ca51fc7e506
Jacob Bare <jacob@limit0.io>
2020-11-13 11:50:24 -0600

    Add social share icons to content page

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
+import SocialSharing from '@base-cms/marko-web-social-sharing/browser';
 
 export default (Browser) => {
   DefaultTheme(Browser);
   GTM(Browser);
   GAM(Browser);
+  SocialSharing(Browser);
 };
