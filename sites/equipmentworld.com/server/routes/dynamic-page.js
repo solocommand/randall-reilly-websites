d2593ff5ca9d4b201f7b5c5c63947f8f9714c99e
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-12 18:35:23 -0600

    Update dynamic page routes

--- sites/equipmentworld.com/server/routes/dynamic-page.js
+++ sites/equipmentworld.com/server/routes/dynamic-page.js
@@ -1,6 +1,6 @@
 const { withDynamicPage } = require('@base-cms/marko-web/middleware');
-const page = require('@randall-reilly/package-shared/templates/dynamic-page');
-const queryFragment = require('@randall-reilly/package-shared/graphql/fragments/dynamic-page');
+const queryFragment = require('@randall-reilly/package-global/graphql/fragments/dynamic-page');
+const page = require('@randall-reilly/package-global/templates/dynamic-page');
 
 module.exports = (app) => {
   app.get('/page/:alias(*)', withDynamicPage({
