f5cac807d6d071b922c24e881467e1f0b7b27bc6
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-12 19:00:08 -0600

    Update website-section routes

--- sites/totallandscapecare.com/server/routes/website-section.js
+++ sites/totallandscapecare.com/server/routes/website-section.js
@@ -1,23 +1,8 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
-const queryFragment = require('@randall-reilly/package-shared/graphql/fragments/website-section-page');
-
-const section = require('../templates/website-section');
-const channel = require('../templates/website-section/channel');
-
-const channelAliases = [
-  'news',
-  'how-to',
-  'design-build',
-  'equipment',
-  'business',
-  'lawncare',
-];
+const queryFragment = require('@randall-reilly/package-global/graphql/fragments/website-section-page');
+const section = require('@randall-reilly/package-global/templates/website-section');
 
 module.exports = (app) => {
-  app.get(`/:alias(${channelAliases.join('|')})`, withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
--- sites/truckersnews.com/server/routes/website-section.js
+++ sites/truckersnews.com/server/routes/website-section.js
@@ -1,26 +1,8 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
-const queryFragment = require('@randall-reilly/package-shared/graphql/fragments/website-section-page');
-
-const section = require('../templates/website-section');
-const channel = require('../templates/website-section/channel');
-
-const channelAliases = [
-  'trucks',
-  'news',
-  'features',
-  'fun',
-  'whats-hot',
-  'videos',
-  'jobs',
-  'she-drives',
-  'gear',
-];
+const queryFragment = require('@randall-reilly/package-global/graphql/fragments/website-section-page');
+const section = require('@randall-reilly/package-global/templates/website-section');
 
 module.exports = (app) => {
-  app.get(`/:alias(${channelAliases.join('|')})`, withWebsiteSection({
-    template: channel,
-    queryFragment,
-  }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
