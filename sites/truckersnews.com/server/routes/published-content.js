0c5962d0d93108d035a9d7ad1a797584f14d8199
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-12 18:57:24 -0600

    Delete unused routes

--- sites/truckersnews.com/server/routes/published-content.js
+++
@@ -1,9 +0,0 @@
-const webinars = require('@randall-reilly/package-shared/templates/published-content/webinars');
-const events = require('@randall-reilly/package-shared/templates/published-content/events');
-const videos = require('@randall-reilly/package-shared/templates/published-content/videos');
-
-module.exports = (app) => {
-  app.get('/events', (_, res) => { res.marko(events); });
-  app.get('/webinars', (_, res) => { res.marko(webinars, { title: 'Webinars' }); });
-  app.get('/videos', (_, res) => { res.marko(videos); });
-};
