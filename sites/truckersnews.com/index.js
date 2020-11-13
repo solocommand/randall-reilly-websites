96cc71d55eff86def126a29a2d3e7cf60a4b963d
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-12 18:33:50 -0600

    Update index.js

--- sites/truckersnews.com/index.js
+++ sites/truckersnews.com/index.js
@@ -1,4 +1,4 @@
-const startServer = require('@randall-reilly/package-shared/start-server');
+const startServer = require('@randall-reilly/package-global/start-server');
 
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
