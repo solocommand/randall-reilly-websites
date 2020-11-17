2de17a7d27782be067d284f4e980c780ed11cf7b
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-16 18:43:38 -0600

    Disable lazy loading, add wallpaper and interstitial units

--- sites/ccjdigital.com/config/gam.js
+++ sites/ccjdigital.com/config/gam.js
@@ -3,7 +3,7 @@ const configureGAM = require('@randall-reilly/package-global/config/gam');
 const config = configureGAM({ basePath: 'ccj' });
 
 config.lazyLoad = {
-  enabled: true, // set to true to enable lazy loading
+  enabled: false, // set to true to enable lazy loading
   fetchMarginPercent: 100, // fetch ad when one viewport away
   renderMarginPercent: 50, // render ad when half viewport away
   mobileScaling: 2, // double these on mobile
@@ -13,6 +13,8 @@ config
   .setAliasAdUnits('default', [
     { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'leaderboard' },
     { name: 'rotation', templateName: 'ROTATION', path: 'rotation' },
+    { name: 'wallpaper', path: 'wallpaper' },
+    { name: 'interstitial', path: 'interstitial' },
   ]);
 
 module.exports = config;
