33910b24ef87e890c5169d2fddb449d660ac1f0c
Jacob Bare <jacob@parameter1.com>
2020-11-25 16:53:31 -0600

    Create button mixin; set primary styles; move vars into own files

--- sites/overdriveonline.com/config/site.js
+++ sites/overdriveonline.com/config/site.js
@@ -32,6 +32,6 @@ module.exports = {
   },
   newsletterSignupBanner: {
     href: '#',
-    description: 'Sign up for the Overdrive Daily to keep up with trucking news, equipment and business information.',
+    description: 'Sign up for the <span class="newsletter-name">Overdrive Daily</span> to keep up with trucking news, equipment and business information.',
   },
 };
