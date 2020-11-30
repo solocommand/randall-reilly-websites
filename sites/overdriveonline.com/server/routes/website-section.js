c951bb11baec331e0dae826e765e71babf67bac1
Jacob Bare <jacob@parameter1.com>
2020-11-30 13:12:18 -0600

    Create latest products feed block

--- sites/overdriveonline.com/server/routes/website-section.js
+++ sites/overdriveonline.com/server/routes/website-section.js
@@ -1,11 +1,11 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const queryFragment = require('@randall-reilly/package-global/graphql/fragments/website-section-page');
 const section = require('@randall-reilly/package-global/templates/website-section');
-const gear = require('@randall-reilly/package-global/templates/website-section/gear');
+const productList = require('@randall-reilly/package-global/templates/website-section/product-list');
 
 module.exports = (app) => {
   app.get('/:alias(gear)', withWebsiteSection({
-    template: gear,
+    template: productList,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
