060b7d890c8757081342b803e85668bc4d930589
Jacob Bare <jacob@parameter1.com>
2020-11-30 12:52:55 -0600

    Create section feed block and apply to section page

---
+++ packages/global/graphql/fragments/section-feed-block.js
@@ -0,0 +1,28 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+
+fragment SectionFeedBlockContentFragment on Content {
+  id
+  type
+  shortName
+  teaser(input: { useFallback: false, maxLength: null })
+  siteContext {
+    path
+  }
+  published
+  primarySection {
+    id
+    name
+    fullName
+    canonicalPath
+  }
+  primaryImage {
+    id
+    src
+    alt
+    isLogo
+  }
+}
+
+`;
