d25fd7b2cbfe28b933d331f0da14bb2935608f66
Jacob Bare <jacob@parameter1.com>
2020-12-01 13:17:05 -0600

    Create default website section layout and use on OVD

--- sites/overdriveonline.com/server/routes/website-section.js
+++ sites/overdriveonline.com/server/routes/website-section.js
@@ -1,7 +1,7 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const queryFragment = require('@randall-reilly/package-global/graphql/fragments/website-section-page');
-const section = require('@randall-reilly/package-global/templates/website-section');
 const productList = require('@randall-reilly/package-global/templates/website-section/product-list');
+const section = require('../templates/website-section');
 
 module.exports = (app) => {
   app.get('/:alias(gear)', withWebsiteSection({
