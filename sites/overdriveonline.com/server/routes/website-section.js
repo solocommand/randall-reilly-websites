8e452776661d96b317c0f048ca258d41dd128e42
Jacob Bare <jacob@parameter1.com>
2020-12-01 13:59:23 -0600

    Create section feed and products layout

--- sites/overdriveonline.com/server/routes/website-section.js
+++ sites/overdriveonline.com/server/routes/website-section.js
@@ -1,11 +1,11 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const queryFragment = require('@randall-reilly/package-global/graphql/fragments/website-section-page');
-const productList = require('@randall-reilly/package-global/templates/website-section/product-list');
+const products = require('../templates/website-section/products');
 const section = require('../templates/website-section');
 
 module.exports = (app) => {
   app.get('/:alias(gear)', withWebsiteSection({
-    template: productList,
+    template: products,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
