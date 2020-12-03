6203456c9a60d89592f32cc69f33143f194117cd
Jacob Bare <jacob@parameter1.com>
2020-12-02 18:35:29 -0600

    Create own toggle buttons

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,11 +1,15 @@
-import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
 import SocialSharing from '@base-cms/marko-web-social-sharing/browser';
+import MenuToggleButton from './menu-toggle-button.vue';
+import NewsletterNub from './newsletter-nub.vue';
+
 
 export default (Browser) => {
-  DefaultTheme(Browser);
   GTM(Browser);
   GAM(Browser);
   SocialSharing(Browser);
+
+  Browser.register('GlobalMenuToggleButton', MenuToggleButton);
+  Browser.register('GlobalNewsletterNub', NewsletterNub);
 };
---
+++ packages/global/browser/newsletter-nub.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :class="className"
+    type="button"
+    :aria-label="buttonLabel"
+    @click="toggle"
+  >
+    <icon-chevron-up :modifiers="iconModifiers" />
+  </button>
+</template>
+
+<script>
+import IconChevronUp from '@base-cms/marko-web-icons/browser/chevron-up.vue';
+
+export default {
+  components: {
+    IconChevronUp,
+  },
+  props: {
+    className: {
+      type: String,
+      default: null,
+    },
+    targetButton: {
+      type: String,
+      required: true,
+    },
+    iconModifiers: {
+      type: Array,
+      default: () => [],
+    },
+    buttonLabel: {
+      type: String,
+      default: 'Toggle Newsletter Menu',
+    },
+  },
+  methods: {
+    toggle() {
+      const element = document.querySelector(this.targetButton);
+      if (element) element.click();
+    },
+  },
+};
+</script>
