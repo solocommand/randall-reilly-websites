fd81d716115949868a7622e2d127b608a472671d
Josh Worden <josh@parameter1.com>
2020-12-03 19:30:59 -0600

    Enable wallpaper ad units

--- sites/overdriveonline.com/config/gam.js
+++ sites/overdriveonline.com/config/gam.js
@@ -20,7 +20,8 @@ config
     { name: 'rotation', templateName: 'ROTATION', path: 'rotation' },
     { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'rotation' },
     { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
-    { name: 'wallpaper', path: 'wallpaper' },
+    { name: 'wallpaper-left', templateName: 'WALLPAPER', path: 'wallpaper-left' },
+    { name: 'wallpaper-right', templateName: 'WALLPAPER', path: 'wallpaper-right' },
     { name: 'interstitial', path: 'interstitial' },
   ])
   .setAliasAdUnits('business', [
@@ -28,8 +29,6 @@ config
     { name: 'rotation', templateName: 'ROTATION', path: 'business-rotation' },
     { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'business-rotation' },
     { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'business-rotation' },
-    { name: 'wallpaper', path: 'wallpaper' },
-    { name: 'interstitial', path: 'interstitial' },
   ])
   .setAliasAdUnits('gear', [
     { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'gear-leaderboard' },
@@ -37,8 +36,6 @@ config
     { name: 'rotation-1', templateName: 'GEAR', path: 'gear-rotation-1' },
     { name: 'rotation-2', templateName: 'GEAR', path: 'gear-rotation-2' },
     { name: 'rotation-3', templateName: 'GEAR', path: 'gear-rotation-3' },
-    { name: 'wallpaper', path: 'wallpaper' },
-    { name: 'interstitial', path: 'interstitial' },
   ]);
 
 module.exports = config;
