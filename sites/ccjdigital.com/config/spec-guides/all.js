c7e03982eaf475224de8611c49f6724e16d1291e
Brian Miller <brian@parameter1.com>
2020-12-17 16:28:57 -0600

    fix teaser and description in config

--- sites/ccjdigital.com/config/spec-guides/all.js
+++ sites/ccjdigital.com/config/spec-guides/all.js
@@ -116,8 +116,8 @@ const getTableRows = async () => {
 
 module.exports = {
   title: 'CCJ Top 250',
-  teaser: 'Get all the info on the top 250 trucking companies in America',
-  description: '<h2>Get the data how you want it</h2> <p>Want to sort the CCJ Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Want a copy of the CCJ Top 250 data sent to you in a PDF file? All of that is available completely free by providing the requested information below.</p> <p>By clicking the button below, I agree to the Terms & Conditions and Privacy Policy, and consent to receive automated marketing calls or text messages from Randall-Reilly. Consent is not a condition of any purchase. You may opt out at any time.</p>',
+  teaser: 'Want to sort the CCJ Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Want a copy of the CCJ Top 250 data sent to you in a PDF file? All of that is available completely free by providing the requested information below.',
+  description: '<div clas="mb-block"><p><strong>Get the data how you want it!</strong></p> <p>Want to sort the CCJ Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Want a copy of the CCJ Top 250 data sent to you in a PDF file?</p></div>',
   downloadLink: '/##########',
   tableRows: getTableRows(),
   columns,
--- sites/ccjdigital.com/config/spec-guides/index.js
+++ sites/ccjdigital.com/config/spec-guides/index.js
@@ -3,7 +3,8 @@ const all = require('./all');
 module.exports = {
   rootAlias: 'ccj-top-250',
   title: 'CCJ Top 250',
-  description: 'Want to sort the CCJ Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Want a copy of the CCJ Top 250 data sent to you in a PDF file? All of that is available completely free by providing the requested information below.',
+  teaser: 'Want to sort the CCJ Top 250 by revenues or number of trucks, tractors, trailers or drivers? Want to see and sort carriers by type of haul or geographic region? Want a copy of the CCJ Top 250 data sent to you in a PDF file? All of that is available completely free by providing the requested information below.',
+  description: '',
   guides: {
     all,
     'dedicated-contract-carriage': {
