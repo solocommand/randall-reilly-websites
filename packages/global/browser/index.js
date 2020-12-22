05cce3f4031e1f686369d97431cb4ba1f63a8ce3
Josh Worden <josh@parameter1.com>
2020-12-22 13:20:58 -0600

    Add brower components

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,6 +1,7 @@
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
 import SocialSharing from '@base-cms/marko-web-social-sharing/browser';
+import IdentityX from '@base-cms/marko-web-identity-x/browser';
 import BlockLoader from './block-loader.vue';
 import MenuToggleButton from './menu-toggle-button.vue';
 import NewsletterNub from './newsletter-nub.vue';
@@ -12,6 +13,7 @@ export default (Browser) => {
   GTM(Browser);
   GAM(Browser);
   SocialSharing(Browser);
+  IdentityX(Browser);
 
   Browser.register('GlobalBlockLoader', BlockLoader);
   Browser.register('GlobalMenuToggleButton', MenuToggleButton);
