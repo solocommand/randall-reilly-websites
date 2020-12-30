3e9c92ab3931344e49d448460ff16770a973a375
Brian Miller <brian@parameter1.com>
2020-12-30 16:42:05 -0600

    fix lint error

--- packages/global/routes/print-content.js
+++ packages/global/routes/print-content.js
@@ -3,7 +3,6 @@ const print = require('../templates/content/print');
 const queryFragment = require('../graphql/fragments/content-page');
 
 module.exports = (app) => {
-
   app.get('/print/content/:id(\\d{8})', withContent({
     template: print,
     queryFragment,
