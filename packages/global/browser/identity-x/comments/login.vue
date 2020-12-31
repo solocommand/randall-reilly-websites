94a6699e637134a4ece967cc3b63b19b3c3f1dde
Jacob Bare <jacob@parameter1.com>
2020-12-31 10:01:09 -0500

    Create custom identity-x comment stream

---
+++ packages/global/browser/identity-x/comments/login.vue
@@ -0,0 +1,181 @@
+<template>
+  <div v-if="hasActiveUser">
+    <p>You are currently logged in as {{ activeUser.email }}.</p>
+    <a
+      :href="endpoints.logout"
+      class="btn btn-primary"
+      role="button"
+    >
+      {{ buttonLabels.logout }}
+    </a>
+  </div>
+  <div v-else-if="complete">
+    <h4>Almost Done!</h4>
+    <p>
+      We just sent an email to <em>{{ email }}</em> with your one-time login link.
+      To finish logging in, open the email message and click the link within.
+    </p>
+    <p>
+      Note: please check your spam/junk folders.
+      If you do not receive this email, your firewall or ISP has likely blocked it.
+      Please add noreply@identity-x.parameter1.com to your whitelist and try registering again.
+    </p>
+  </div>
+  <div v-else>
+    <form @submit.prevent="handleSubmit">
+      <fieldset :disabled="loading">
+        <email v-model="email" />
+        <small
+          v-if="consentPolicy"
+          class="text-muted mb-3"
+          v-html="consentPolicy"
+        />
+        <button
+          type="submit"
+          class="btn btn-primary"
+        >
+          {{ buttonLabels.submit }}
+        </button>
+      </fieldset>
+      <p v-if="error" class="mt-3 text-danger">
+        An error occurred: {{ error.message }}
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+import Email from '@base-cms/marko-web-identity-x/browser/form/fields/email.vue';
+import cleanPath from '@base-cms/marko-web-identity-x/browser/utils/clean-path';
+import post from '@base-cms/marko-web-identity-x/browser/utils/post';
+import cookiesEnabled from '@base-cms/marko-web-identity-x/browser/utils/cookies-enabled';
+import FormError from '@base-cms/marko-web-identity-x/browser/errors/form';
+import FeatureError from '@base-cms/marko-web-identity-x/browser/errors/feature';
+
+export default {
+  /**
+   *
+   */
+  components: {
+    Email,
+  },
+
+  /**
+   *
+   */
+  props: {
+    activeUser: {
+      type: Object,
+      default: () => {},
+    },
+    endpoints: {
+      type: Object,
+      required: true,
+    },
+    buttonLabels: {
+      type: Object,
+      default: () => ({
+        submit: 'Login / Register',
+        logout: 'Logout',
+      }),
+    },
+    consentPolicy: {
+      type: String,
+      default: null,
+    },
+    redirect: {
+      type: String,
+      default: null,
+    },
+    appContextId: {
+      type: String,
+      default: null,
+    },
+  },
+
+  /**
+   *
+   */
+  data: () => ({
+    email: null,
+    complete: false,
+    error: null,
+    loading: false,
+  }),
+
+  /**
+   *
+   */
+  computed: {
+    /**
+     *
+     */
+    authUrl() {
+      return `${window.location.origin}/${cleanPath(this.endpoints.authenticate)}`;
+    },
+
+    /**
+     *
+     */
+    hasActiveUser() {
+      return this.activeUser && this.activeUser.email;
+    },
+
+    /**
+     *
+     */
+    redirectTo() {
+      const { redirect } = this;
+      if (redirect) return redirect;
+      const { pathname, search, hash } = window.location;
+      return `${pathname}${search}${hash}`;
+    },
+  },
+
+  /**
+   *
+   */
+  mounted() {
+    if (!cookiesEnabled()) {
+      this.error = new FeatureError('Your browser does not support cookies. Please enable cookies to use this feature.');
+    }
+  },
+
+  /**
+   *
+   */
+  methods: {
+    /**
+     *
+     */
+    async handleSubmit() {
+      this.error = null;
+      this.loading = true;
+      const {
+        email,
+        redirectTo,
+        authUrl,
+        appContextId,
+      } = this;
+      try {
+        const res = await post('/login', {
+          email,
+          redirectTo,
+          authUrl,
+          appContextId,
+        });
+        const data = await res.json();
+        if (!res.ok) throw new FormError(data.message, res.status);
+        if (data.ok) {
+          this.complete = true;
+          this.$emit('submit');
+        }
+      } catch (e) {
+        this.error = e;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+};
+</script>
---
+++ packages/global/browser/identity-x/comments/stream.vue
@@ -0,0 +1,260 @@
+<template>
+  <div :class="classNames">
+    <div :class="element('header')">
+      {{ headerText }}
+    </div>
+
+    <div :class="element('body')">
+      <div
+        v-if="hasActiveUser"
+        :class="element('create-post')"
+      >
+        <div v-if="archived" :class="element('archived')">
+          This thread is archived and is no longer accepting comments.
+        </div>
+        <create
+          v-else
+          :display-name="activeUser.displayName"
+          :stream="{ identifier, title, description, url }"
+          @complete="load"
+        />
+      </div>
+      <div v-else :class="element('login-form-wrapper')">
+        <p :class="element('login-message')">
+          You must be signed in to leave a comment.
+        </p>
+        <p :class="element('login-message')">
+          To sign in or create an account,
+          enter your email address and we'll send you a one-click sign-in link.
+        </p>
+        <login
+          :class="element('login-form')"
+          :active-user="activeUser"
+          :endpoints="endpoints"
+          :consent-policy="consentPolicy"
+          :app-context-id="appContextId"
+          :button-labels="{ submit: 'Continue', logout: 'Logout' }"
+        />
+      </div>
+
+      <h5 v-if="latestCommentsHeader && comments.length" :class="element('latest-comments')">
+        {{ latestCommentsHeader }}
+      </h5>
+    </div>
+    <div v-if="isLoading" :class="element('loading')">
+      Loading comments...
+    </div>
+    <div v-else-if="error" :class="element('error')">
+      Unable to load comments: {{ error.message }}
+    </div>
+    <div v-else-if="comments.length === 0" :class="element('no-posts')">
+      {{ noCommentsMessage }}
+    </div>
+    <div v-else :class="element('posts')">
+      <div
+        v-for="comment in comments"
+        :key="comment.id"
+        :class="element('post')"
+      >
+        <post
+          :id="comment.id"
+          :body="comment.body"
+          :display-name="comment.user.displayName"
+          :created-at="comment.createdAt"
+          :approved="comment.approved"
+          :flagged="comment.flagged"
+          :date-format="dateFormat"
+          :active-user="activeUser"
+          @reported="load"
+        />
+      </div>
+      <div v-if="hasNextPage" :class="element('post')">
+        <button class="btn btn-secondary" :disabled="isLoadingMore" @click.prevent="loadMore">
+          <span v-if="isLoadingMore">Loading...</span>
+          <span v-else>Show More Comments</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import get from '@base-cms/marko-web-identity-x/browser/utils/get';
+import Post from '@base-cms/marko-web-identity-x/browser/comments/post.vue';
+import Login from './login.vue';
+import Create from './create.vue';
+
+export default {
+  /**
+   *
+   */
+  components: { Login, Post, Create },
+
+  /**
+   *
+   */
+  props: {
+    activeUser: {
+      type: Object,
+      default: () => {},
+    },
+    identifier: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: undefined,
+    },
+    description: {
+      type: String,
+      default: undefined,
+    },
+    url: {
+      type: String,
+      default: undefined,
+    },
+    endpoints: {
+      type: Object,
+      required: true,
+    },
+    headerText: {
+      type: String,
+      default: 'Leave a Comment',
+    },
+    latestCommentsHeader: {
+      type: String,
+      default: 'Comments',
+    },
+    noCommentsMessage: {
+      type: String,
+      default: 'No comments have been added yet. Want to start the conversation?',
+    },
+    modifiers: {
+      type: Array,
+      default: () => [],
+    },
+    consentPolicy: {
+      type: String,
+      default: null,
+    },
+    dateFormat: {
+      type: String,
+      default: undefined,
+    },
+    appContextId: {
+      type: String,
+      default: null,
+    },
+  },
+
+  data: () => ({
+    blockName: 'idx-comment-stream',
+    isLoading: false,
+    isLoadingMore: false,
+    isLoginFormDisplayed: false,
+    error: null,
+    comments: [],
+    archived: false,
+    hasNextPage: false,
+    endCursor: null,
+    totalCount: 0,
+  }),
+
+  computed: {
+    /**
+     *
+     */
+    classNames() {
+      const { blockName } = this;
+      const classNames = [blockName];
+      this.modifiers.map(mod => classNames.push(`${blockName}--${mod}`));
+      return classNames;
+    },
+
+    /**
+     *
+     */
+    hasActiveUser() {
+      return this.activeUser && this.activeUser.email;
+    },
+  },
+
+  /**
+   *
+   */
+  created() {
+    this.load();
+  },
+
+  /**
+   *
+   */
+  methods: {
+    /**
+     *
+     */
+    element(name) {
+      return `${this.blockName}__${name}`;
+    },
+
+    /**
+     *
+     */
+    showLoginForm() {
+      this.isLoginFormDisplayed = true;
+    },
+
+    /**
+     *
+     */
+    hideLoginForm() {
+      this.isLoginFormDisplayed = false;
+    },
+
+    /**
+     *
+     */
+    async load() {
+      this.error = null;
+      this.isLoading = true;
+      try {
+        const res = await get(`/comments/${this.identifier}`);
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.message);
+        this.totalCount = data.totalCount;
+        this.comments = data.edges.map(edge => edge.node);
+        this.hasNextPage = data.pageInfo.hasNextPage;
+        this.endCursor = data.pageInfo.endCursor;
+        this.archived = data.stream ? data.stream.archived : false;
+      } catch (e) {
+        this.error = e;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+
+    /**
+     *
+     */
+    async loadMore() {
+      this.error = null;
+      this.isLoadingMore = true;
+      try {
+        const res = await get(`/comments/${this.identifier}?after=${this.endCursor}`);
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.message);
+        this.totalCount = data.totalCount;
+        const comments = data.edges.map(edge => edge.node);
+        this.comments.push(...comments);
+        this.hasNextPage = data.pageInfo.hasNextPage;
+        this.endCursor = data.pageInfo.endCursor;
+      } catch (e) {
+        this.error = e;
+      } finally {
+        this.isLoadingMore = false;
+      }
+    },
+  },
+};
+</script>
