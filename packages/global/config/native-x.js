44445f70d68e11dafb97e122a4746937ade359f1
Brian Miller <brian@parameter1.com>
2021-01-05 08:51:29 -0600

    fix native x uri & enable nativeX for ovd

--- packages/global/config/native-x.js
+++ packages/global/config/native-x.js
@@ -1,6 +1,6 @@
 const NativeXConfiguration = require('@base-cms/marko-web-native-x/config');
 
 module.exports = ({
-  uri = 'https://randall-reilly.native-x.parameter1.com',
+  uri = 'https://randallreilly.native-x.parameter1.com',
   enabled = false,
 } = {}) => new NativeXConfiguration(uri, { enabled });
