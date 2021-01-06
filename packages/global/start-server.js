7189724f02831c8e9d1c4c38d8aa1701367894bb
Jacob Bare <jacob@parameter1.com>
2021-01-06 11:54:04 -0500

    Add base browse API middleware to global package

--- packages/global/start-server.js
+++ packages/global/start-server.js
@@ -2,12 +2,16 @@ const newrelic = require('newrelic');
 const { startServer } = require('@base-cms/marko-web');
 const { set, get } = require('@base-cms/object-path');
 const cleanResponse = require('@base-cms/marko-core/middleware/clean-marko-response');
+const baseBrowse = require('@randall-reilly/base-browse/middleware');
 
+const pkg = require('./package.json');
 const document = require('./components/document');
 const components = require('./components');
 const fragments = require('./fragments');
 const sharedRoutes = require('./routes');
 const paginated = require('./middleware/paginated');
+
+const { BASE_BROWSE_GRAPHQL_URI, TENANT_KEY } = process.env;
 
 const routes = siteRoutes => (app) => {
   // Shared/global routes (all sites)
@@ -28,7 +32,15 @@ module.exports = (options = {}) => {
       if (typeof onStart === 'function') await onStart(app);
       app.set('trust proxy', 'loopback, linklocal, uniquelocal');
 
-      // Use pagianted middleware
+      // Use "Base Browse" GraphQL middleware
+      // Will be available on `req.$baseBrowse` and `res.locals.$baseBrowse`
+      app.use(baseBrowse({
+        uri: BASE_BROWSE_GRAPHQL_URI,
+        tenantKey: TENANT_KEY,
+        config: { name: pkg.name, version: pkg.version },
+      }));
+
+      // Use paginated middleware
       app.use(paginated());
 
       // Setup GAM.
