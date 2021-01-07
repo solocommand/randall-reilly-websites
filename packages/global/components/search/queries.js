b6a024ee34f402db6ed9043a9bd6f25614e3d17d
Jacob Bare <jacob@parameter1.com>
2021-01-06 23:04:33 -0500

    Use algolia search in query component

--- packages/global/components/search/queries.js
+++ packages/global/components/search/queries.js
@@ -1,39 +1,6 @@
 const gql = require('graphql-tag');
 const { extractFragmentData } = require('@base-cms/web-common/utils');
 
-const BASE_BROWSE_QUERY = gql`
-  query WebsiteSearchPage(
-    $siteId: ObjectID!
-    $now: Date!
-    $phrase: String!
-    $limit: Int = 18
-    $skip: Int = 0
-  ) {
-    browseContent(input: {
-      pagination: { limit: $limit, skip: $skip }
-      match: { fields: [NAME, TEASER], phrase: $phrase }
-      sort: { field: PUBLISHED, order: DESC }
-      statuses: [PUBLISHED]
-      schedules: {
-        website: {
-          siteIds: [$siteId]
-          dates: [
-            { field: START, before: $now }
-            { field: END, after: $now, includeMissingValues: true }
-          ]
-        }
-      }
-    }) {
-      totalCount
-      edges {
-        node {
-          id
-        }
-      }
-    }
-  }
-`;
-
 const buildContentQuery = ({ fragment }) => {
   const { spreadFragmentName, processedFragment } = extractFragmentData(fragment);
   return gql`
@@ -45,7 +12,6 @@ const buildContentQuery = ({ fragment }) => {
         ids: $ids,
         status: any,
         pagination: { limit: $limit }
-        sort: { field: published, order: desc }
       }) {
         edges {
           node {
@@ -59,7 +25,4 @@ const buildContentQuery = ({ fragment }) => {
   `;
 };
 
-module.exports = {
-  BASE_BROWSE_QUERY,
-  buildContentQuery,
-};
+module.exports = { buildContentQuery };
