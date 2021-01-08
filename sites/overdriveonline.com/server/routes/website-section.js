50e2808d4142c7bbfbed72b6a44aa223cff2e5c4
Brandon Krigbaum <brandonbk@gmail.com>
2021-01-07 19:24:44 -0600

    Add reader-rigs route, template

--- sites/overdriveonline.com/server/routes/website-section.js
+++ sites/overdriveonline.com/server/routes/website-section.js
@@ -2,6 +2,7 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const queryFragment = require('@randall-reilly/package-global/graphql/fragments/website-section-page');
 const partnersInBusiness = require('../templates/website-section/partners-in-business');
 const products = require('../templates/website-section/products');
+const readerRigs = require('../templates/website-section/reader-rigs');
 const rrSubmission = require('../templates/new-reader-rigs-submission');
 const section = require('../templates/website-section');
 
@@ -13,6 +14,10 @@ module.exports = (app) => {
   }));
   app.get('/:alias(partners-in-business)', withWebsiteSection({
     template: partnersInBusiness,
+    queryFragment,
+  }));
+  app.get('/:alias(reader-rigs)', withWebsiteSection({
+    template: readerRigs,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
