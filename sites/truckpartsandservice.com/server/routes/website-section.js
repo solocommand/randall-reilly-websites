dbe92ff4fe6eaa08935e7a4d16ab4248c42be8c0
Jacob Bare <jacob@parameter1.com>
2021-01-27 15:25:00 -0600

    Create rough directory layout and index+section pages

--- sites/truckpartsandservice.com/server/routes/website-section.js
+++ sites/truckpartsandservice.com/server/routes/website-section.js
@@ -3,7 +3,21 @@ const queryFragment = require('@randall-reilly/package-global/graphql/fragments/
 const products = require('../templates/website-section/products');
 const section = require('../templates/website-section');
 
+const directoryIndex = require('../templates/website-section/directory');
+const directorySection = require('../templates/website-section/directory/_alias');
+
+const directoryAlias = 'aftermarket-truck-parts-suppliers';
+
 module.exports = (app) => {
+  app.get(`/:alias(${directoryAlias})`, withWebsiteSection({
+    template: directoryIndex,
+    queryFragment,
+  }));
+  app.get(`/:alias(${directoryAlias}/[a-z0-9-/]+)`, withWebsiteSection({
+    template: directorySection,
+    queryFragment,
+  }));
+
   app.get('/:alias(products)', withWebsiteSection({
     template: products,
     queryFragment,
