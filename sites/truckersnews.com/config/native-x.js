c63d584cf03648a73563e754e7c567399ae102fd
Brandon Krigbaum <brandonbk@gmail.com>
2021-02-04 13:48:24 -0600

    Enable native-x for remaining sites

--- sites/truckersnews.com/config/native-x.js
+++ sites/truckersnews.com/config/native-x.js
@@ -2,6 +2,8 @@ const configureNativeX = require('@randall-reilly/package-global/config/native-x
 
 const config = configureNativeX();
 
+config.enabled = true;
+
 config
   .setAliasPlacements('default', [
     { name: 'default', id: '6000b7460ea4830001340b96' },
