75e7d1749f8d98fb221aa74d4691783f3a1a4f8d
Brandon Krigbaum <brandonbk@gmail.com>
2021-03-02 06:52:24 -0600

    Add graphql query

--- packages/global/routes/feed.js
+++ packages/global/routes/feed.js
@@ -1,6 +1,71 @@
 const { asyncRoute } = require('@parameter1/base-cms-utils');
+const gql = require('graphql-tag');
 module.exports = (app) => {
   app.get('/feed', asyncRoute(async (req, res) => {
+    const FEED = gql`
+      query Feed($input: WebsiteScheduledContentQueryInput = {}, $siteId: ObjectID!) {
+        websiteSite(input: { id: $siteId }) {
+          id
+          shortName
+          name
+          url
+        }
+        websiteScheduledContent(input: $input) {
+          totalCount
+          edges {
+            node {
+              id
+              name
+              teaser
+              body
+              publishedDate(input: { format: "ddd, DD MMM YYYY hh:mm:ss ZZ" })
+              labels
+              siteContext {
+                url
+              }
+              company {
+                id
+                name
+              }
+              primarySite {
+                shortName
+              }
+              primaryImage {
+                id
+                src(input: { options: { auto: "format,compress", q: 70 } })
+                alt
+                isLogo
+              }
+              ... on Authorable {
+                authors {
+                  edges {
+                    node {
+                      id
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const limit = 10;
+    const skip = 0;
+    const input = {
+      includeContentTypes: ['Article'],
+      pagination: {
+        limit,
+        skip,
+      },
+    };
+
+    const siteId = req.app.locals.config.website('id');
+    const variables = { input, siteId };
+    const { data } = await req.apollo.query({ query: FEED, variables });
+    const { websiteSite: site } = data;
     const rssAttributes = [
       'version="2.0"',
       'xmlns:content="http://purl.org/rss/1.0/modules/content/"',
