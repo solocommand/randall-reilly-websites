1c4c603b933822de63580aba7ef976bf2feea26e
Brandon Krigbaum <brandonbk@gmail.com>
2021-03-14 15:46:24 -0500

    Update HWT newsletter config

--- sites/hardworkingtrucks.com/config/newsletter.js
+++ sites/hardworkingtrucks.com/config/newsletter.js
@@ -1,30 +1,30 @@
 const baseConfig = {
   action: 'https://randallreilly.dragonforms.com/loading.do',
   hiddenInputs: [
-    { name: 'omedasite', value: 'ov_subscriptions' },
+    { name: 'omedasite', value: 'hwt_subscriptions' },
   ],
-  href: 'https://randallreilly.dragonforms.com/loading.do?omedasite=ov_subscriptions',
+  href: 'https://randallreilly.dragonforms.com/loading.do?omedasite=hwt_subscriptions',
 };
 
 module.exports = {
   signupBanner: {
     ...baseConfig,
-    description: 'Get the business tips, industry insights and trending news every owner-operator needs to know in the <span class="newsletter-name">Overdrive</span> newsletter.',
+    description: 'Get the business tips, industry insights and trending news every truck owner needs to know in the <em>Hard Working Trucks</em> newsletter.',
   },
   signupBannerLarge: {
     ...baseConfig,
     name: 'Don’t Miss Out',
-    description: 'Get the business tips, industry insights and trending news every owner-operator needs to know in the <span class="newsletter-name">Overdrive</span> newsletter.',
+    description: 'Get the business tips, industry insights and trending news every truck owner needs to know in the <em>Hard Working Trucks</em> newsletter.',
   },
   signupFooter: {
     ...baseConfig,
-    name: 'Newsletter Just for Owner-Operators',
-    description: 'Get owner-operator news and insights, plus diesel prices and gear reviews — delivered straight to your inbox.',
+    name: 'Hardworking Trucks Daily',
+    description: 'Sign up for the <span class="newsletter-name">Hardworking Trucks Daily</span> to keep up with...',
   },
   pushdown: {
     ...baseConfig,
-    imagePath: 'static/iphone-x-mockup-noshadow-2x.png',
-    name: 'Don’t Miss Out',
-    description: 'Join 135,000 owner-operators who get helpful insights and important news delivered straight to their inbox with the <span class="newsletter-name">Overdrive</span> newsletter.',
+    imagePath: 'static/newsletter-pushdown/hwt-half.png',
+    name: 'Newsletter Just for Truck Owners',
+    description: 'Get news you need to know about pickups, commercial vans and Class 3-8 trucks — delivered straight to your inbox.',
   },
 };
